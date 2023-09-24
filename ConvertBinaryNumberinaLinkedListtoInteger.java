ed11469c4c444eb664cff15804f2bd6da89e22c7
kunalpaliwal13 <143526414+kunalpaliwal13@users.noreply.github.com>
2023-09-24 07:51:38 +0530

    1290. Convert Binary Number in a Linked List to Integer #day25

---
+++ ConvertBinaryNumberinaLinkedListtoInteger.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ 
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        String sum ="";
+        ListNode i = head;
+        while(i!=null){
+            
+            sum =sum+ i.val;
+            i= i.next;
+        }
+        int res = Integer.parseInt(sum, 2);
+        return res;
+    
+    
+    }
+}
