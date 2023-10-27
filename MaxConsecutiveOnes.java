b324cde2c83bd725d87076b9657e6993240c4ed4
kunalpaliwal13 <143526414+kunalpaliwal13@users.noreply.github.com>
2023-10-28 00:13:16 +0530

    Update MaxConsecutiveOnes.java

--- MaxConsecutiveOnes.java
+++ MaxConsecutiveOnes.java
@@ -4,9 +4,10 @@ class Solution {
         int max=0;
         for( int i=0; i< nums.length; i++){
             if(nums[i]==1){count ++;}
-            if( nums[i]==0){if(count>max){max= count;count =0;}}
+            if( nums[i]==0){max= Math.max(count, max); count =0;}
         }
-        if(count>max){max= count;count =0;}
+        max= Math.max(count, max);
+     
     return max;
     }
 }
