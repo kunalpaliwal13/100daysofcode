a6c783305e4f5d7d294abe678f10ad5637d33f86
Kunal Paliwal <kunalpaliwal2003@gmail.com>
2024-01-13 12:25:34 +0530

    Update MaximumBinaryTree.java

--- MaximumBinaryTree.java
+++ MaximumBinaryTree.java
@@ -22,27 +22,7 @@ class Solution {
         }
         return ind;
     }
-
-    public void createLeftNode(TreeNode root, int[] nums){
-        int ind = returnInd(nums);
-        int[] num1 = Arrays.copyOfRange(nums, 0, ind);
-        int[] num2 = Arrays.copyOfRange(nums, ind+1, nums.length);
-        //ind --> 3
-        root.left  = new TreeNode(nums[ind]);
-        root.left.left = constructMaximumBinaryTree(num1);
-        root.left.right = constructMaximumBinaryTree(num2);
-    }
-    public void createRightNode(TreeNode root, int[] nums){
-        int ind = returnInd(nums);
-        int[] num1 = Arrays.copyOfRange(nums, 0, ind);
-        int[] num2 = Arrays.copyOfRange(nums, ind+1, nums.length);
-        //ind --> 3
-        root.right  = new TreeNode(nums[ind]);
-        root.right.left = constructMaximumBinaryTree(num1);
-        root.right.right = constructMaximumBinaryTree(num2);
-    }
- 
-
+    
 
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         if( nums.length ==0){return null;}
@@ -52,8 +32,8 @@ class Solution {
         int[] num1 = Arrays.copyOfRange(nums, 0, ind);
         int[] num2 = Arrays.copyOfRange(nums, ind+1, nums.length);
         TreeNode root = new TreeNode(nums[ind]);
-        if(num1.length !=0){createLeftNode(root, num1);}
-        if(num2.length !=0){createRightNode(root, num2);}
+        root.left = constructMaximumBinaryTree(num1);
+        root.right= constructMaximumBinaryTree(num2);
         return root;
 
 
