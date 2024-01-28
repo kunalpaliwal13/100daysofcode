9bb55cc33de0300547197a66cccfaf612803904d
Kunal Paliwal <kunalpaliwal2003@gmail.com>
2024-01-28 15:00:32 +0530

    219. Contains Duplicate II

---
+++ ContainsDuplicateII.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums.length ==0){return false;}
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for( int i=0;i< nums.length; i++){
+            if(hm.containsKey(nums[i]) && Math.abs(i - hm.get(nums[i]))<=k){return true;}
+            else{hm.put(nums[i], i);}
+        }
+        return false;
+    }
+}
